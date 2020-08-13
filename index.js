8f813bb8a75916e488206bd7ea9cb6bf98872cbd
Jake Champion <me@jakechampion.name>
2020-08-13 12:35:57 +0100

    Restore index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var success = () => process.exit(0)
 var eventHandlers = createObjectWithDefaultValue({}, function () {
 	throw new Error(
 		`
-The Origami project board action should only be run on \`pull_request\`, \`push\` or
-\`issues\`, but was run on \`${github.context.eventName}\`.
-See https://github.com/Financial-Times/origami-project-board-action#usage`
+The Origami project board action only works on \`pull_request\` or
+\`issues\` events, not for i\`${github.context.eventName}\`.
+Please see https://github.com/Financial-Times/origami-project-board-action#usage for information on how to set up the workflow correctly`
 	)
 })
 
@@ -30,33 +30,6 @@ function handleCreateCardResponse (response) {
 	}
 
 	throw new Error(JSON.stringify(response))
-}
-
-eventHandlers.push = async function push (octo, {incomingColumnId}) {
-	if (github.context.payload.pull_request) {
-		core.info("it's an push, but it's also a PR i'll let the PR action handle it:)")
-		return
-	}
-
-    var result = await octo.repos.listPullRequestsAssociatedWithCommit({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        commit_sha: github.context.sha,
-    })
-
-    var pr = result.data.find(el => el.state === 'open')
-
-	var id = pr && pr.number
-
-	core.info(`creating card for pr ${id}, ${JSON.stringify(result.data)}`)
-
-	return octo.projects.createCard({
-		column_id: incomingColumnId,
-		content_id: id,
-		content_type: contentTypes.pr
-	})
-		.then(handleCreateCardResponse)
-		.catch(handleCreateCardResponse)
 }
 
 eventHandlers.pull_request = async function pullRequest (octo, {incomingColumnId}) {
@@ -106,9 +79,7 @@ eventHandlers.issues = async function issues (octo, {incomingColumnId}) {
 
 void async function () {
 	try {
-		var origamiFoxAccessToken = core.getInput("origami-fox-access-token", {
-			required: true
-		})
+		var origamiFoxAccessToken = core.getInput("origami-fox-access-token")
 		var incomingColumnId = core.getInput("incoming-column-id")
 		var octo = new github.GitHub(origamiFoxAccessToken, {
 			previews: ["inertia-preview"]
